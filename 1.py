492946fa64581c69c87fe49f7b49782eff7161ea
huzaifah16 <35855812+huzaifah16@users.noreply.github.com>
2022-07-16 23:16:17 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -40,51 +40,57 @@ def lemmatization(texts,allowed_postags=['NOUN', 'ADJ']):
     return output
 
 
-with open(r'corpus.pkl', 'rb') as q:
-    review_data_00_05, review_data_06_10, review_data_11_15, review_data_16_20  = pickle.load(q)   
+    
+file_00_05 = open(r'https://github.com/huzaifah16/LDA_multicore/blob/f76339482b007e05ab476df043b7f67f5ad0287a/corpus_00_05.pkl', 'rb')
+file_06_10 = open(r'https://github.com/huzaifah16/LDA_multicore/blob/f76339482b007e05ab476df043b7f67f5ad0287a/corpus_06_10.pkl', 'rb')
+file_11_15 = open(r'https://github.com/huzaifah16/LDA_multicore/blob/f76339482b007e05ab476df043b7f67f5ad0287a/corpus_11_15.pkl', 'rb')
+file_16_20 = open(r'https://github.com/huzaifah16/LDA_multicore/blob/f76339482b007e05ab476df043b7f67f5ad0287a/corpus_16_20.pkl', 'rb')
 
-    text_list_00_05=review_data_00_05['Data'].tolist()
-    text_list_06_10=review_data_06_10['Data'].tolist()
-    text_list_11_15=review_data_11_15['Data'].tolist()
-    text_list_16_20=review_data_16_20['Data'].tolist()
+review_data_00_05 = pickle.load(file_00_05)
+review_data_06_10 = pickle.load(file_06_10)
+review_data_11_15 = pickle.load(file_11_15)
+review_data_16_20 = pickle.load(file_16_20)
+   
 
-    text_list_rem_00_05 = prep_clean(text_list_00_05)
-    text_list_rem_06_10 = prep_clean(text_list_06_10)
-    text_list_rem_11_15 = prep_clean(text_list_11_15)
-    text_list_rem_16_20 = prep_clean(text_list_16_20)
-    print("text_list_rem_16_20 = prep_clean(text_list_16_20)")
-    tokenized_reviews_00_05 = lemmatization(text_list_rem_00_05)
-    tokenized_reviews_06_10 = lemmatization(text_list_rem_06_10)
-    tokenized_reviews_11_15 = lemmatization(text_list_rem_11_15)
-    tokenized_reviews_16_20 = lemmatization(text_list_rem_16_20)
-    print("tokenized_reviews_16_20 = lemmatization(text_list_rem_16_20)")
-    dictionary_00 = corpora.Dictionary(tokenized_reviews_00_05)
-    dictionary_06 = corpora.Dictionary(tokenized_reviews_06_10)
-    dictionary_11 = corpora.Dictionary(tokenized_reviews_11_15)
-    dictionary_16 = corpora.Dictionary(tokenized_reviews_16_20)
-    print("    dictionary_16 = corpora.Dictionary(tokenized_reviews_16_20)")
-    doc_term_matrix_00 = [dictionary_00.doc2bow(rev) for rev in tokenized_reviews_00_05]
-    doc_term_matrix_06 = [dictionary_06.doc2bow(rev) for rev in tokenized_reviews_06_10]
-    doc_term_matrix_11 = [dictionary_11.doc2bow(rev) for rev in tokenized_reviews_11_15]
-    doc_term_matrix_16 = [dictionary_16.doc2bow(rev) for rev in tokenized_reviews_16_20]
-
-    # Creating the object for LDA model using gensim library
-    LDA = gensim.models.ldamulticore
-
-    print("Build LDA model1")
-    lda_model_00 = LDA(corpus=doc_term_matrix_00, id2word=dictionary_00, num_topics=10, random_state=100,
+text_list_00_05=review_data_00_05['Data'].tolist()
+text_list_06_10=review_data_06_10['Data'].tolist()
+text_list_11_15=review_data_11_15['Data'].tolist()
+text_list_16_20=review_data_16_20['Data'].tolist()
+text_list_rem_00_05 = prep_clean(text_list_00_05)
+text_list_rem_06_10 = prep_clean(text_list_06_10)
+text_list_rem_11_15 = prep_clean(text_list_11_15)
+text_list_rem_16_20 = prep_clean(text_list_16_20)
+print("text_list_rem_16_20 = prep_clean(text_list_16_20)")
+tokenized_reviews_00_05 = lemmatization(text_list_rem_00_05)
+tokenized_reviews_06_10 = lemmatization(text_list_rem_06_10)
+tokenized_reviews_11_15 = lemmatization(text_list_rem_11_15)
+tokenized_reviews_16_20 = lemmatization(text_list_rem_16_20)
+print("tokenized_reviews_16_20 = lemmatization(text_list_rem_16_20)")
+dictionary_00 = corpora.Dictionary(tokenized_reviews_00_05)
+dictionary_06 = corpora.Dictionary(tokenized_reviews_06_10)
+dictionary_11 = corpora.Dictionary(tokenized_reviews_11_15)
+dictionary_16 = corpora.Dictionary(tokenized_reviews_16_20)
+print("    dictionary_16 = corpora.Dictionary(tokenized_reviews_16_20)")
+doc_term_matrix_00 = [dictionary_00.doc2bow(rev) for rev in tokenized_reviews_00_05]
+doc_term_matrix_06 = [dictionary_06.doc2bow(rev) for rev in tokenized_reviews_06_10]
+doc_term_matrix_11 = [dictionary_11.doc2bow(rev) for rev in tokenized_reviews_11_15]
+doc_term_matrix_16 = [dictionary_16.doc2bow(rev) for rev in tokenized_reviews_16_20]
+# Creating the object for LDA model using gensim library
+LDA = gensim.models.ldamulticore
+print("Build LDA model1")
+lda_model_00 = LDA(corpus=doc_term_matrix_00, id2word=dictionary_00, num_topics=10, random_state=100,
                 chunksize=1000, passes=50,iterations=100)
-    print("Build LDA model2")
-    lda_model_06 = LDA(corpus=doc_term_matrix_06, id2word=dictionary_06, num_topics=10, random_state=100,
+print("Build LDA model2")
+lda_model_06 = LDA(corpus=doc_term_matrix_06, id2word=dictionary_06, num_topics=10, random_state=100,
                 chunksize=1000, passes=50,iterations=100)
-    print("Build LDA model3")
-    lda_model_11 = LDA(corpus=doc_term_matrix_11, id2word=dictionary_11, num_topics=10, random_state=100,
+print("Build LDA model3")
+lda_model_11 = LDA(corpus=doc_term_matrix_11, id2word=dictionary_11, num_topics=10, random_state=100,
                 chunksize=1000, passes=50,iterations=100)
-    print("Build LDA model4")
-    lda_model_16 = LDA(corpus=doc_term_matrix_16, id2word=dictionary_16, num_topics=10, random_state=100,
+print("Build LDA model4")
+lda_model_16 = LDA(corpus=doc_term_matrix_16, id2word=dictionary_16, num_topics=10, random_state=100,
                 chunksize=1000, passes=50,iterations=100)
 
 
-    file = open('done.pkl', 'wb')
-    pickle.dump((lda_model_00,lda_model_06,lda_model_11,lda_model_16, doc_term_matrix_00, doc_term_matrix_06, doc_term_matrix_11, doc_term_matrix_16, dictionary_00, dictionary_06, dictionary_11, dictionary_16), file)
-    file.close()
+file = open('done.pkl', 'wb')
+pickle.dump((lda_model_00,lda_model_06,lda_model_11,lda_model_16, doc_term_matrix_00, doc_term_matrix_06, doc_term_matrix_11, doc_term_matrix_16, dictionary_00, dictionary_06, dictionary_11, dictionary_16), file)
+file.close()
